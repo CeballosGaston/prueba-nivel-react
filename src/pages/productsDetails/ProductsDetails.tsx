ac3e9650cceb255eedb880af46d528ca64c623b1
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-03-25 11:41:11 +0100

    feat: products details added

---
+++ src/pages/productsDetails/ProductsDetails.tsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom";
+import { albums } from "../../data/albums";
+import type { Albums } from "../../types";
+import { useNavigate } from "react-router-dom";
+
+const ProductsDetails = () => {
+  const navigate = useNavigate();
+  const { albumId } = useParams();
+  const album: Albums | undefined = albums.find((a) => a.id === albumId);
+
+  if (!album) return <div>Album Not Found</div>;
+
+  return (
+    <div>
+      <div>
+        <h1>Album: {album.album}</h1>
+        <h2>Singer Name: {album.name}</h2>
+        <h2>Song: {album.song}</h2>
+        <h2>Year: {album.year}</h2>
+        <h2>Description: {album.description}</h2>
+      </div>
+      <button onClick={() => navigate("/")}>Come back to Products</button>
+    </div>
+  );
+};
+
+export default ProductsDetails;
