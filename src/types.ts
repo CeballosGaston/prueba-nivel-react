39382658dafa5885f714db46919c4fd054de1128
Gastón Ceballos <ceballoscallerosgaston@gmail.com>
2026-03-25 11:16:06 +0100

    files structure, data and types added.

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type Albums = {
+  id: string;
+  name: string;
+  song: string;
+  album: string;
+  year: string;
+  country: string;
+  image: string;
+  rating: number;
+  description: string;
+};
